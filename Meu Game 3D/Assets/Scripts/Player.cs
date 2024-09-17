dccf535fa1001d1263853b1372e12e2ea90ff91d
Kaynho <p.kayo@escolar.ifrn.edu.br>
2024-09-17 15:08:03 -0300

    labirinto

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,28 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
-    public Rigidbody rb;
+    public int forcaPulo = 7;
+    public bool noChao;
+
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("start");
+        Debug.Log("START");
         TryGetComponent(out rb);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!noChao &&  collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("update");
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        Debug.Log("UPDATE");
+        float h = Input.GetAxis("Horizontal"); //-1 esquerda,0 nada, 1 direita
+        float v = Input.GetAxis("Vertical"); // -1 pra tras, 0 nada, 1 pra frente
 
-        UnityEngine.Vector3 direcao = new Vector3(x, 0, y);
-        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+         
+        Vector3 direcao = new Vector3(h, 0, v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+
+        if(Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+
+
+
+        if (transform.position.y <= -10)
+        {
+            //jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+
+
     }
 }
